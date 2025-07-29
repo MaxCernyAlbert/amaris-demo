d5a0cac65e447d0ca7b5da6fd5ed571ae7f4a4e7
Maxim Černý <info@maxinthecloud.com>
2025-07-30 00:34:07 +0200

    backend fix (#2)

--- envs/prod/backend.tf
+++ envs/prod/backend.tf
@@ -1,9 +1,4 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-operation"
-    storage_account_name = "stamaristerraformprod"
-    container_name       = "tfstate"
-    key                  = "prod.tfstate"
-    use_oidc             = true
-  }
-}
+resource_group_name  = "rg-operation"
+storage_account_name = "stamaristerraformprod"
+container_name       = "tfstate"
+key                  = "prod.tfstate"
